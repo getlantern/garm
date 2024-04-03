559028ecbf3417087729cb7bc07f9ba3d91b5259
Eli Yukelzon <reflog@gmail.com>
2024-04-03 17:26:46 +0200

    no uuid in mysql

--- database/sql/models.go
+++ database/sql/models.go
@@ -27,7 +27,7 @@ import (
 )
 
 type Base struct {
-	ID        uuid.UUID `gorm:"type:uuid;primary_key;"`
+	ID        uuid.UUID `gorm:"type:char(36);primary_key;"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
